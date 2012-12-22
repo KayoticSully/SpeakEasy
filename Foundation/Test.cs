620b98a4cf4a0688b0ded00beadc225e1144c6f9
Ryan Sullivan <KayoticSully@gmail.com>
2012-12-22 04:45:05 -0500

    Started the Foundation Plugin

---
+++ Foundation/Test.cs
@@ -0,0 +1,17 @@
+﻿using SpeakEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation
+{
+    class Test : Plugin
+    {
+        public void ping()
+        {
+            speak("pong");
+        }
+    }
+}
